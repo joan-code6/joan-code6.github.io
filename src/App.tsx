fbb523c634cc7bd2c11ed477b1deda2ed4c61a04
Joan Code <none>
2026-03-22 16:38:00 +0100

    Update Router basename to use environment variable

--- src/App.tsx
+++ src/App.tsx
@@ -5,7 +5,7 @@ import './App.css'
 
 function App() {
   return (
-    <Router basename="/Portfolio-v-2/">
+    <Router basename={import.meta.env.BASE_URL}>
       <Routes>
         <Route path="/" element={<Home />} />
         <Route path="/portfolio" element={<Portfolio />} />
